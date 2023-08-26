8d626e067fc952ba0127f346b402791093051a20
yellhtutaung <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>
2023-08-26 22:38:19 +0630

    home code

--- includes/functions.php
+++ includes/functions.php
@@ -1,3 +1,3 @@
 <?php
-//    $domainUrl = 'http://localhost/futurenow_php';
-    $domainUrl = 'http://192.168.0.106/futurenow_php';
+    $domainUrl = 'http://localhost/futurenowmm_php';
+//    $domainUrl = 'http://192.168.0.106/futurenow_php';
